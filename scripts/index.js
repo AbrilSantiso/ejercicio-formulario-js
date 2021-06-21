d620dd8777fc57d383844ad8530b4daae2efdb52
Abril <abrilsantiso@hotmail.com>
2021-06-21 11:34:50 -0300

    first-commit

--- scripts/index.js
+++ scripts/index.js
@@ -12,4 +12,34 @@ registerBtn.addEventListener("click", () => {
   // un mensaje de error: "Debes aceptar los TyCs para registrarte en el sitio"
   // 7 Si todo esta correcto, mostramos por consola un objeto con la información
   // ingresada por el usuario.
+  var email = document.getElementById("email-input").value;
+  var password = document.getElementById("password-input").value;
+  var legalAge = document.getElementsByName("legalAge");
+  var termsAndConditions = document.getElementById("tyc-input");
+  function checkingAge() {
+    let check = null;
+    legalAge.forEach(function (radio) {
+      if (radio.id == "age_no" && radio.checked) {
+        alert("Debes ser mayor de edad para registrarte en el sitio");
+        check = "false";
+      } else {
+        check = "true";
+      }
+    });
+    return check;
+  }
+  function checkingTerms() {
+    if (!termsAndConditions.checked) {
+      window.alert("Debes aceptar los TyCs para registrarte en el sitio");
+    }
+    return termsAndConditions.checked;
+  }
+
+  let data = {
+    email: email,
+    password: password,
+    legalAge: checkingAge(),
+    tycAccepted: checkingTerms(),
+  };
+  console.log(data);
 });
